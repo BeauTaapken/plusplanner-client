c9e27ce5add5d25fccbc5da3b2aefb5ce279f29e
Roy <roy.appeldoorn@gmail.com>
2019-10-21 12:35:14 +0200

    Edit

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,11 @@ module.exports = {
     'style-resources-loader': {
       preProcessor: 'scss',
       patterns: [
-        path.resolve(__dirname, 'src/styles/abstracts/*.scss')
+        path.resolve(__dirname, 'src/styles/abstracts/*.scss'),
+        path.resolve(__dirname, 'src/styles/base/*.scss'),
+        path.resolve(__dirname, 'src/styles/components/*.scss'),
+        path.resolve(__dirname, 'src/styles/pages/*.scss'),
+        path.resolve(__dirname, 'src/styles/vendors/*.scss'),
       ]
     }
   }
