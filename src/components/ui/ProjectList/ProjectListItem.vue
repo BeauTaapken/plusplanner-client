f12e6c6da258d4845e45ce1097ed6af1381f84d9
BlackMania <BlackMania@users.noreply.github.com>
2019-10-28 13:27:58 +0100

    Fixed the selected project on refresh from page.

--- src/components/ui/ProjectList/ProjectListItem.vue
+++ src/components/ui/ProjectList/ProjectListItem.vue
@@ -1,15 +1,14 @@
 <template>
-        <li>
-            <div class="box" @click="setActive">
-                <router-link style="color: white; text-decoration: none" :key="projectName" :to="{ name: 'project', params: {projectName: projectName } }">
+    <li>
+        <div @click="setActive" class="box">
+            <router-link :key="projectName" :to="{ name: 'project', params: {projectName: projectName } }"
+                         style="color: white; text-decoration: none">
                 <div class="content">
-                    <div class="contentText" @click="setActive">
-                        {{ projectName }}
-                    </div>
+                    <div @click="setActive" class="contentText">{{ projectName }}</div>
                 </div>
-                </router-link>
-            </div>
-        </li>
+            </router-link>
+        </div>
+    </li>
 </template>
 
 <script>
@@ -22,7 +21,7 @@
             this.checkActive();
         },
         methods: {
-            setActive: function(event) {
+            setActive: function (event) {
                 let allBoxes = document.getElementsByClassName("content");
                 let allTextBoxes = document.getElementsByClassName("contentText");
                 for (let e = 0; e < allBoxes.length; e++) {
@@ -32,66 +31,76 @@
                 event.target.classList.add("selected");
                 event.target.parentElement.classList.add("selected");
             },
-            checkActive: function() {
-                if(this.projectName === this.$route.params.projectName)
+            checkActive: function () {
+                let allProjects = document.getElementsByClassName('box');
+
+                if(allProjects.length < 0)
                 {
-                    document.getElementsByClassName('contentText')[0].classList.add("selected");
-                    document.getElementsByClassName('content')[0].classList.add("selected");
+                    return;
+                }
+                for(let i = 0; i < allProjects.length; i++)
+                {
+                    if(allProjects[i].getElementsByClassName('contentText')[0].innerHTML === this.$route.params.projectName)
+                    {
+                        allProjects[i].getElementsByClassName('contentText')[0].classList.add('selected');
+                        allProjects[i].getElementsByClassName('content')[0].classList.add('selected');
+                    }
                 }
             }
-
         }
     }
 </script>
 
 <style scoped>
-li {
-    text-decoration: none;
-    list-style-type: none;
-    text-align: center;
-}
-.content {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    line-height:100%;
-    height:100%;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-}
+    li {
+        text-decoration: none;
+        list-style-type: none;
+        text-align: center;
+    }
 
-.contentText {
-    transition: all 1s ease;
-    font-family: monospace;
-    font-size: 20px;
-    width: 1ch;
-    overflow: hidden;
-    white-space: nowrap;
-}
+    .content {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        line-height: 100%;
+        height: 100%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
 
-.box {
-    transition: all 1s ease;
-    background-color: #2E3136;
-    position: relative;
-    width: 60%;
-    margin-left: 20%;
-    margin-right: 20%;
-    margin-top: 5%;
-    border-radius: 50px;
-}
-.box:before {
-    content: "";
-    display: block;
-    padding-top: 100%;
-}
+    .contentText {
+        transition: all 1s ease;
+        font-family: monospace;
+        font-size: 20px;
+        width: 1ch;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+    .box {
+        transition: all 1s ease;
+        background-color: #2E3136;
+        position: relative;
+        width: 60%;
+        margin-left: 20%;
+        margin-right: 20%;
+        margin-top: 5%;
+        border-radius: 50px;
+    }
+
+    .box:before {
+        content: "";
+        display: block;
+        padding-top: 100%;
+    }
 
     .box:hover {
         border-radius: 15px;
