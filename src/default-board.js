81cec9838e990adbb116e4f0a4d48c5cdb3cf30a
Roy <roy.appeldoorn@gmail.com>
2019-11-04 12:29:45 +0100

    added board

---
+++ src/default-board.js
@@ -0,0 +1,99 @@
+import { uuid } from './utils';
+
+export default {
+    name: 'workshop',
+    columns: [
+        {
+            name: 'todo',
+            tasks: [
+                {
+                    description: '',
+                    name: 'first task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'second task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'and third',
+                    id: uuid(),
+                    userAssigned: null
+                },
+            ]
+        },
+        {
+            name: 'in-progress',
+            tasks: [
+                {
+                    description: '',
+                    name: 'first task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'second task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'and third',
+                    id: uuid(),
+                    userAssigned: null
+                },
+            ]
+        },
+        {
+            name: 'working on',
+            tasks: [
+                {
+                    description: '',
+                    name: 'first task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'second task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'and third',
+                    id: uuid(),
+                    userAssigned: null
+                },
+            ]
+        },
+        {
+            name: 'test',
+            tasks: [
+                {
+                    description: '',
+                    name: 'first task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'second task',
+                    id: uuid(),
+                    userAssigned: null
+                },
+                {
+                    description: '',
+                    name: 'and third',
+                    id: uuid(),
+                    userAssigned: null
+                },
+            ]
+        }
+    ]
+}
