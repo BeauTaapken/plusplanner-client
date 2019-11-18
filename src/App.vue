b250f8a3ff087d50ef98286148717f273a29d1a2
BlackMania <BlackMania@users.noreply.github.com>
2019-11-18 15:25:10 +0100

    Added API call and added if statement to load ProjectList when the data is loaded

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <ProjectList></ProjectList>
+    <ProjectList v-if="isLoaded"></ProjectList>
     <router-view :key="this.$route.fullPath"></router-view>
     <router-view :key="this.$route.params.contentName" name="content" />
     <router-view :key="this.$route.params.contentName" name="chat" />
@@ -10,12 +10,25 @@
 <script>
 import ProjectList from "./views/ProjectList/ProjectList";
 import json from "./components/Projectfiles";
+import apiService from "./services/APIService";
+
 export default {
   components: { ProjectList },
   data() {
     return {
-      projectData: json
+      projectData: json,
+      isLoaded: false
     };
+  },
+  created() {
+    apiService.getProjects()
+    // eslint-disable-next-line no-console
+            .then(response => {
+              this.projectData = response.data
+              this.isLoaded = true;
+            })
+            // eslint-disable-next-line no-console
+                    .catch(error => console.log("There was an error: " + error.response));
   }
 };
 </script>
