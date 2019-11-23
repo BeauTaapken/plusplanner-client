96c5a2c0fa785ad80f58c12c3f8405d46f8abeab
Vincent <vincentmuijtjens@ziggo.nl>
2019-11-23 17:27:22 +0100

    Added CreateTask to add an item to the table

---
+++ src/components/Board/CreateTask.vue
@@ -0,0 +1,119 @@
+<template>
+    <div>
+        <v-card @click="addTask" color="grey" >
+            <v-layout justify-center="true"  class="v-card-item">
+                <v-card-actions>
+                    <v-card-title>{{name}}</v-card-title>
+                </v-card-actions>
+            </v-layout>
+        </v-card>
+
+        <v-overlay
+
+                :value="overlay"
+        >
+            <v-card class="createTask">
+                <v-form>
+                    <v-container>
+                        <v-card-title class="justify-center">New task</v-card-title>
+                        <v-text-field
+                                id="taskname"
+                                label="Task name"
+                                filled
+                                shaped
+                        ></v-text-field>
+
+
+                        <v-text-field
+                                id="taskdescription"
+                                label="Description"
+                                filled
+                                shaped
+                        ></v-text-field>
+                        <v-menu>
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                    v-model="date"
+                                    label="End Date"
+                                    readonly
+                                    v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="date" @input="menu2 = false" width="426" color="#424242" event-color="#424242"></v-date-picker>
+                        </v-menu>
+
+                        <v-row>
+                            <v-col cols="6">
+                                <v-btn width="100%" color="#4f4f4f" @click="cancel">
+                                    Cancel
+                                </v-btn>
+                            </v-col>
+
+                            <v-col cols="6">
+                                <v-btn width="100%" color="#3b3b3b" @click="save">
+                                    Add
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-form>
+            </v-card>
+        </v-overlay>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CreateTask",
+        props: {
+            name: String,
+            tableName: String
+        },
+        data() {
+            return {
+                overlay: false,
+                date: new Date().toISOString().substr(0, 10),
+                menu2: false,
+            }
+        },
+        methods: {
+            addTask: function () {
+                this.overlay = !this.overlay;
+            },
+            cancel: function () {
+                this.overlay = !this.overlay;
+            },
+            save: function() {
+                let name = document.getElementById('taskname').value;
+                let description = document.getElementById('taskdescription').value;
+                let componentId = this.$parent.$parent.$parent.componentId;
+                let json = `{ "subpartname": "${name}", "description": "${description}", "state": "${this.tableName}", "enddate": "${this.date}", "partid": "${componentId}" }`;
+                this.$parent.$parent.itemArray.push(JSON.parse(json));
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .createTask {
+        padding: 5%;
+        width: 500px;
+        height: auto;
+        overflow: hidden;
+    }
+
+    >>> .v-text-field input {
+        text-align: center
+    }
+
+    .v-card-item {
+        margin-top: 2%;
+
+        transition: background-color 1s ease;
+    }
+
+    .v-card-item:hover {
+        cursor: pointer;
+        background-color: #5e5e5e;
+    }
+</style>
