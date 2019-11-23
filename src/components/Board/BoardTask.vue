8fb89bcf9f8838b00d5e322caa022e237087d2b0
Vincent <vincentmuijtjens@ziggo.nl>
2019-11-23 17:27:50 +0100

    Fixed so that the tasks will also update in the parents components array

--- src/components/Board/BoardTask.vue
+++ src/components/Board/BoardTask.vue
@@ -1,6 +1,6 @@
 <template>
-        <v-card color="grey" class="v-card-item">
-            <v-layout justify-center="true">
+        <v-card color="grey" >
+            <v-layout justify-center="true" class="v-card-item">
                 <v-card-actions>
                     <v-card-title>{{name}}</v-card-title>
                 </v-card-actions>
@@ -9,17 +9,24 @@
 </template>
 
 <script>
+
+
     export default {
         name: "BoardTask",
         props: {
             endDate: String,
             name: String
-        }
+        },
     }
 </script>
 
 <style scoped>
     .v-card-item {
         margin-top: 2%;
+        transition: background-color 1s ease;
+    }
+    .v-card-item:hover {
+        cursor: pointer;
+        background-color: #5e5e5e;
     }
 </style>
