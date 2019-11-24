57da940304f6ca82ac401a5ec5ed68f2e0792cce
Roy <roy.appeldoorn@gmail.com>
2019-11-24 20:33:13 +0100

    prevented overlay drag

--- src/views/SubMenu/SubMenu.vue
+++ src/views/SubMenu/SubMenu.vue
@@ -1,80 +1,84 @@
 <template>
-    <v-navigation-drawer fixed class="project-nav">
-        <v-list>
-                <v-list-group >
-                    <template v-slot:activator>
-                        <v-list-item-title class="project-header">{{ projectName }}</v-list-item-title>
-                    </template>
-                </v-list-group>
-        </v-list>
-        <SubMenuHeader
-                v-for="data in this.usedData.components"
-                v-bind:key="data.componentname"
-                v-bind:component-name="data.componentname"
-                v-bind:component-item="data.parts"
-        />
-        <SubMenuChatHeader
-                v-for="data in this.usedData.chats"
-                v-bind:key="data.chatid"
-                v-bind:component-name="data.name"
-                v-bind:component-item="data.channels"
-        />
-    </v-navigation-drawer>
+  <v-navigation-drawer fixed class="project-nav">
+    <v-list>
+      <v-list-group>
+        <template v-slot:activator>
+          <v-list-item-title class="project-header">{{
+            projectName
+          }}</v-list-item-title>
+        </template>
+      </v-list-group>
+    </v-list>
+    <SubMenuHeader
+      v-for="data in this.usedData.components"
+      v-bind:key="data.componentname"
+      v-bind:component-name="data.componentname"
+      v-bind:component-item="data.parts"
+    />
+    <SubMenuChatHeader
+      v-for="data in this.usedData.chats"
+      v-bind:key="data.chatid"
+      v-bind:component-name="data.name"
+      v-bind:component-item="data.channels"
+    />
+  </v-navigation-drawer>
 </template>
 
 <script>
-    import SubMenuHeader from "./SubMenuHeader";
-    import SubMenuChatHeader from "./SubMenuChatHeader";
+import SubMenuHeader from "./SubMenuHeader";
+import SubMenuChatHeader from "./SubMenuChatHeader";
 
-    export default {
-        name: "SubMenu",
-        components: {SubMenuChatHeader, SubMenuHeader},
-        data: function () {
-            return {
-                usedData: null,
-                projectName: this.$route.params.projectName
-            }
-        },
-        created() {
-            this.load();
-        },
-        updated() {
-            this.load();
-        },
-        methods: {
-            load: function() {
-                let json = this.$parent.$parent.projectData;
-                let res = json.projects.filter(d => d.projectname === this.$route.params.projectName);
-                this.usedData = res[0];
-            }
-        }
+export default {
+  name: "SubMenu",
+  components: { SubMenuChatHeader, SubMenuHeader },
+  data: function() {
+    return {
+      usedData: null,
+      projectName: this.$route.params.projectName
+    };
+  },
+  created() {
+    this.load();
+  },
+  updated() {
+    this.load();
+  },
+  methods: {
+    load: function() {
+      let json = this.$parent.$parent.projectData;
+      let res = json.projects.filter(
+        d => d.projectname === this.$route.params.projectName
+      );
+      this.usedData = res[0];
     }
+  }
+};
 </script>
 
 <style scoped>
-    >>>.v-list-group .v-list-item__title{
-        color: white;
-        height: auto;
-    }
+>>> .v-list-group .v-list-item__title {
+  color: white;
+  height: auto;
+}
 
-    >>>.v-list-group  .v-icon{
-        color: white;
-    }
+>>> .v-list-group .v-icon {
+  color: white;
+}
 
-    >>>.theme--light.v-list {
-        background-color: #2E3136;
-    }
+>>> .theme--light.v-list {
+  background-color: #2e3136;
+}
 
-    .project-nav {
-        background-color: #2E3136;
-        height: 100vh !important;
-        width: 13% !important;
-        float: left !important;
-        margin-left: calc(4% - 1px) !important;
-    }
-    .project-header {
-        font-size: 35px;
-        padding-top: 20px;
-        padding-bottom: 20px;
-    }
+.project-nav {
+  background-color: #2e3136;
+  height: 100vh !important;
+  width: 13% !important;
+  float: left !important;
+  margin-left: calc(4% - 1px) !important;
+}
+.project-header {
+  font-size: 35px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
 </style>
