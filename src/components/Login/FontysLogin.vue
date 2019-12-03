accddddaa66b19ccb9ed927fb7b46a649730686d
Vincent <vincentmuijtjens@ziggo.nl>
2019-12-03 18:16:06 +0100

    Added fontys login button

---
+++ src/components/Login/FontysLogin.vue
@@ -0,0 +1,56 @@
+<template>
+    <button class="loginBtn loginBtn--fontys">
+        Login with Fontys
+    </button>
+</template>
+<script>
+    export default {
+        name: "FontysLogin"
+    }
+</script>
+
+<style scoped>
+    .loginBtn {
+        box-sizing: border-box;
+        position: relative;
+        margin: 0.2em;
+        padding: 0 15px 0 46px;
+        border: none;
+        text-align: left;
+        line-height: 34px;
+        white-space: nowrap;
+        border-radius: 0.2em;
+        font-size: 16px;
+        color: #FFF;
+    }
+    .loginBtn:before {
+        content: "";
+        box-sizing: border-box;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 34px;
+        height: 100%;
+    }
+    .loginBtn:focus {
+        outline: none;
+    }
+    .loginBtn:active {
+        box-shadow: inset 0 0 0 32px rgba(0,0,0,0.1);
+    }
+
+    .loginBtn--fontys {
+        background-color: #673366;
+        background-image: linear-gradient(#673366, #5c2d5b);
+        text-shadow: 0 -1px 0 #354C8C;
+    }
+    .loginBtn--fontys:before {
+        border-right: #4d254c 1px solid;
+        background: url('../../assets/fontys-logo.png') 6px 6px no-repeat;
+    }
+    .loginBtn--fontys:hover,
+    .loginBtn--fontys:focus {
+        background-color: #673366;
+        background-image: linear-gradient(#673366, #5c2d5b);
+    }
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,36 @@
 <template>
     <v-container bg fill-height grid-list-md text-xs-center>
         <v-layout row wrap align-center>
-            <v-card width="50%" style="margin: auto;">
-                <v-card-title>Login</v-card-title>
-                <v-btn>
-                    Fontys login button
-                </v-btn>
+            <v-card width="auto" style="margin: auto;" color="#2E3136" min-width="30%">
+                <v-layout justify-center="true">
+                    <v-card-actions>
+                        <v-card-title>Login</v-card-title>
+                    </v-card-actions>
+                </v-layout>
+                <v-card-actions>
+                    <fontys-login class="auth-button">
+
+                    </fontys-login>
+                </v-card-actions>
             </v-card>
         </v-layout>
     </v-container>
 </template>
 
 <script>
+    import FontysLogin from "../components/Login/FontysLogin";
     export default {
-        name: "Login"
+        name: "Login",
+        components: {FontysLogin}
     }
 </script>
 
 <style scoped>
-#fontys-login {
-    width: inherit;
+    >>>.v-card__title{
+        color: white;
+        text-align: center;
+    }
+.auth-button {
+    margin: auto auto 2%;
 }
 </style>
